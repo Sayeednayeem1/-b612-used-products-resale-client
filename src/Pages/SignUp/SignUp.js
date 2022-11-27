58ded91299efdf6ede3ff04d6363e089eaef1c10
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-27 12:58:25 +0600

    added token related function

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -60,8 +60,19 @@ const SignUp = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
-                navigate('/');
+                userToken(email);
             })
+    };
+
+    const userToken = email =>{
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+        .then( res => res.json())
+        .then( data =>{
+            if (data.accessToken) {
+                localStorage.setItem('accessToken', data.accessToken);
+                navigate('/');
+            }
+        })
     }
 
     return (
