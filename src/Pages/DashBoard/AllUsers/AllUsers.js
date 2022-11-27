24546c661c31d1459cf5395c55d6ebcdd6b24a27
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-27 19:21:19 +0600

    added all users route and data

--- src/Pages/DashBoard/AllUsers/AllUsers.js
+++ src/Pages/DashBoard/AllUsers/AllUsers.js
@@ -1,9 +1,44 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const AllUsers = () => {
+
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await fetch('http://localhost:5000/users');
+            const data = await res.json();
+            return data;
+        }
+    })
+
     return (
         <div>
-            <h5>All users are here!</h5>
+            <h5 className='text-2xl mb-4'>All Users List</h5>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Admin</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user, x )=> <tr key={user._id}>
+                                <th>{x+1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td><button className='btn btn-primary btn-xs'>Make Admin</button></td>
+                                <td><button className='btn btn-xs'>Delete</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
